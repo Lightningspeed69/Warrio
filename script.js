533278cf59737ec26baa8a706d341793c0b11aab
KedarG7 <gurav.kedar2005@gmail.com>
2025-01-08 22:31:28 +0530

    Added a basic Javascript file for storing the data

---
+++ script.js
@@ -0,0 +1,8 @@
+const Signup = {
+    Name : "Kedar Gurav",
+    Email : " gurav.kedar2005@gmail.com",
+    Phone : 1234567890,
+    Password : "   123456",
+    SelectClass : ["Guitar Class", "Keyboard Class", "Tabla Class", "Violin Class"],
+    
+}
